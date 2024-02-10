f69fa655cf06178ca969ca2634a25bef20f82ea9
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-02-10 12:33:48 +0100

    update all

--- learning/learning_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from pygame.locals import *
-import pygame
-from django.contrib.auth.views import LoginView
-# Create your views here.
-def home(request):
-    return render(request,'home.html')
-def mächtigeTricks(request):
-    return render(request, 'tricks.html')
